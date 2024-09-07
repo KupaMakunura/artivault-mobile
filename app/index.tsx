9c62359bcd14c17ff71c09ba0c440a5b0a508e4d
Kupakwashe Makunura <kupsamos@gmail.com>
2024-09-07 06:56:19 +0200

    Add the ui library tamagui

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
-import { Text, View } from "react-native";
+import { TamaguiProvider } from "@tamagui/core";
+import { Button } from "tamagui";
 
-export default function Index() {
+import React from "react";
+
+export default () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <TamaguiProvider>
+      <Button>Click me</Button>
+    </TamaguiProvider>
   );
-}
+};
